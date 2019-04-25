24de06a535f6fa50cb381dc9f469b617967c5058
szendy1 <szendy1@uniba.sk>
2019-04-25 21:49:02 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,15 +3,39 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
+var Maze = [
+  ['000000000000000000000'],
+  ['011111111111111111110'],
+  ['010101111111111101010'],
+  ['000000000000000000000'],
+  ['011111111111111111110'],
+  ['010101111111111101010'],
+  ['000000000000000000000'],
+  ['011111111111111111110'],
+  ['010101111111111101010'],
+  ['000000000000000000000'],
+  ['011111111111111111110'],
+  ['010101111111111101010'],
+]
+
 class Canvas extends React.Component {
   componentDidMount() {
     const canvas = this.refs.canvas
     const ctx = canvas.getContext("2d")
+    for (let i = 0; i< Maze.length;i++){
+      for (let j = 0; j<Maze[i].length;j++){
+        if (Maze[i][j]=='0'){
+          let x = 32*i;
+          let y = 32*j;
+          ctx.fillRect(x,y,x+32,y);
+        }
+      }
+    }
   }
   render() {
     return(
       <div>
-        <canvas ref="canvas" width={640} height={425} />
+        <canvas ref="canvas" width={640} height={448} />
       </div>
     )
   }
