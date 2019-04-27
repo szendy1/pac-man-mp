0068526147667519727b4f3a1d68b4cea1ee6f8f
szendy1 <szendy1@uniba.sk>
2019-04-27 10:56:03 +0200

    1 file modified

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ let gameData = {
   ctx: null,
   gameStarted: false,
   gamePaused: false,
-
+  pacman: null,
+  ghost: null,
 };
 
 
@@ -33,19 +34,43 @@ class Canvas extends React.Component {
     const canvas = document.getElementById('canvas')
     gameData.ctx = canvas.getContext("2d")
     initGame();
-    canvas.onclick = function () { pauseGame() };
+    canvas.onclick = function () { pauseGame(); };
+    canvas.addEventListener('keydown', this.handleKey, false);
   }
+  
+  handleKey(e){
+    switch (e.keyCode) {
+        case 37: alert("Left"); break; //Left key
+        case 38: alert("Up"); break; //Up key
+        case 39: alert("Right"); break; //Right key
+        case 40: alert("Down"); break; //Down key
+    }
+  }
+
   render() {
     return (
       <div>
-        <canvas id="canvas" width={cWidth} height={cHeight} />
+        <canvas id="canvas" onKeyDown={this.handleKey} width={cWidth} height={cHeight} />
       </div>
     )
   }
 }
 
+
+
 function initGame() {
-  gameData.ctx.fillStyle = "#FF0000";
+  initVariables();
+  initCanvas();
+}
+
+function initVariables() {
+  gameData.pacman = new Figure();
+  gameData.ghost = new Figure();
+
+}
+
+function initCanvas() {
+  gameData.ctx.fillStyle = 'black'//"#FF0000";
   gameData.ctx.fillRect(0, 0, 640, 448);
   gameData.ctx.fillStyle = "blue";
   for (let i = 0; i < Maze.length; i++) {
@@ -56,11 +81,21 @@ function initGame() {
         case '0':
           gameData.ctx.fillRect(x, y, tile, tile);
           break;
+        case '1':
+          drawImage(x + 12, y + 12, 'spr_pill_0.png');
+          break;
+        case '2':
+          drawImage(x + 8, y + 8, 'spr_power_pill_0.png');
+          break;
         case 'P':
           drawImage(x, y, 'pac_man_0.png');
+          gameData.pacman.col = j;
+          gameData.pacman.row = i;
           break;
         case 'G':
           drawImage(x, y, 'spr_ghost_orange_0.png');
+          gameData.pacman.col = j;
+          gameData.pacman.row = i;
           break;
       }
     }
@@ -89,26 +124,9 @@ function startGame() {
 
 }
 
-class Game {
-  constructor() {
-    let state = {
-      lives: 3,
-      pac: Pacman(),
-      ghost: Ghost(),
-      map: Maze,
-      pacTiles: [
 
-      ],
-    };
-  }
-  preLoad() {
-
-  }
-}
-
-
-// object constructors
-function Pacman() {
+// object constructor
+function Figure() {
   let row = 0;   // row
   let col = 0;   // col
   let phase = 0;   // animation phase
@@ -119,19 +137,7 @@ function Pacman() {
   let dy = 0;  // delta value for y-movement
   let osx = 0;  // x-offset for smooth animation
   let osy = 0;  // y-offset for smooth animation
-}
-
-function Ghost() {
-  let row = 0;   // row
-  let col = 0;   // col
-  let phase = 0;   // animation phase
-  //this.pn= 0;  // next delta for p (+1 or -1)
-  let dir = 0; // the directions we could go
-  let nextDir = 0;  // the current moving direction
-  let dx = 0;  // delta value for x-movement
-  let dy = 0;  // delta value for y-movement
-  let osx = 0;  // x-offset for smooth animation
-  let osy = 0;  // y-offset for smooth animation
+  let speed = 0;
 }
 
 
