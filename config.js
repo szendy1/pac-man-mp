cd466675f8c1f2d315b08dace20d35c7a97f8c5c
szendy1 <szendy1@uniba.sk>
2019-05-10 13:48:01 +0200

    1 file modified, 1 added

---
+++ config.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore'
+const config = {
+  apiKey: "AIzaSyBB5VGzlVrbGg2VpG_bsikvf8kjeJ8NrDA",
+  authDomain: "pac-man-mp.firebaseapp.com",
+  databaseURL: "https://pac-man-mp.firebaseio.com",
+  projectId: "pac-man-mp",
+  storageBucket: "pac-man-mp.appspot.com",
+  messagingSenderId: "491825287110",
+};
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(config) 
+}
+
+export const database = firebase.firestore()
